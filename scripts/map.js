01579f37406d208b68bada14e7f8f5f7cb7a35ce
Michael <michael@mardr.com>
2021-09-11 22:38:02 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -782,7 +782,7 @@ $(window).on('load', function() {
     var dispTitle = getSetting('_mapTitleDisplay');
 
     if (dispTitle !== 'off') {
-      var usernavigation = '<nobr> ' + usernamegroup + ' | <a href="' + logoutURI + '">logout</a></nobr>';
+      var usernavigation = '<a href="' + logoutURI + '">Logout</a> ' + usernamegroup;
       var title = '<h3 class="pointer">' + getSetting('_mapTitle') + '</h3>';
       var subtitle = '<h5>' + getSetting('_mapSubtitle') + '</h5>';
 
