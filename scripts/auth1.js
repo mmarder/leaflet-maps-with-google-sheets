ff34ec48348d6872302c5fd3b96ec8625f90d736
Michael <michael@mardr.com>
2021-11-13 23:00:16 +0100

    Update auth1.js

--- scripts/auth1.js
+++ scripts/auth1.js
@@ -4,11 +4,15 @@ console.log(url);
 // ?product=shirt&color=blue&newuser&size=m
 const id_token = url.match(/\#(?:id_token)\=([\S\s]*?)\&/);
 
-var username;
 
-const decoded = jwt_decode(id_token[1]);
-username = (decoded["cognito:username"]);
-groups = (decoded["cognito:groups"]);
-sessionStorage.setItem("username", username);
-sessionStorage.setItem("groups", groups);
-usernameUpdate = true;
+if (id_token !== null){
+
+  var username;
+
+  const decoded = jwt_decode(id_token[1]);
+  username = (decoded["cognito:username"]);
+  groups = (decoded["cognito:groups"]);
+  sessionStorage.setItem("username", username);
+  sessionStorage.setItem("groups", groups);
+  usernameUpdate = true;
+};
